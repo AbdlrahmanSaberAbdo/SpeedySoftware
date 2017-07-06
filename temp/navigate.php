6cd3fdb10b425c82b8d3a942de81189b6ce61dec
abdlrahman <abdlrahmansaber2@gmail.com>
2017-07-05 19:20:23 -0700

    updated

---
+++ temp/navigate.php
@@ -0,0 +1,34 @@
+    <header id="header">
+        <nav class="navbar navbar-default navbar-fixed">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+                    <a class="navbar-brand" href="index.php">Spee<span>dy </span>So<span>ft</span>Ware</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php?page=about">About Us<span class="sr-only">(current)</span></a></li>
+                        <li><a href="#">Features</a></li>
+                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                         <a href="#"><i class="fa fa-lg fa-facebook"></i></a>
+                         <a href="#"><i class="fa fa-lg fa-twitter"></i></a>
+                         <a href="#"><i class="fa fa-lg fa-linkedin"></i></a>
+                        <a href="#"><i class="fa fa-lg fa-envelope"></i></a>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
