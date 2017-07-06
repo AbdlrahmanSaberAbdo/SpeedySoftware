ad950a324ccb329dbe066dbc84fee8d48259299b
mahmoodahmad100 <mahmood073@gmail.com>
2017-07-06 22:22:48 +0200

    rendering about us page content

--- about.php
+++ about.php
@@ -1,9 +1,50 @@
  <?php include('temp/header.php'); ?> 
-    <section class="cover2">
-        <?php include('temp/main-cont.php'); ?>
-    </section>
+<section class="cover2">
+    <?php include('temp/main-cont.php'); ?>
+</section>
 
-    <header id="header">
-        <?php include('temp/navigate.php'); ?>
-    </header>
+<header id="header">
+    <?php include('temp/navigate.php'); ?>
+</header>
+
+    <!-- ====================== About Us Content ====================== -->
+    <div class="container">
+        
+        <!-- speedy software -->
+        <b class="title text-center">speedy software</b>
+        <div class="row">
+            <div class="col-md-12">
+                <p>
+                Cras blandit commodo vestibulum. Morbi volutpat pulvinar interdum. Aenean luctus nibh nec tempus dignissim. Etiam vestibulum metus ac placerat volutpat. Aliquam sed hendrerit justo. Sed faucibus justo quis aliquet tempus. Pellentesque volutpat viverra arcu a posuere. Phasellus aliquet consectetur felis, a bibendum ipsum consequat et. Donec venenatis dolor leo, nec dapibus tellus porta in. Sed laoreet aliquam magna, et varius arcu sollicitudin vel. Nunc ut nisl sapien. Pellentesque viverra dignissim nisi ac porta. Quisque eu massa imperdiet nisl vehicula faucibus. 
+                </p>
+            </div>
+        </div>
+        <!-- end speedy software -->
+
+        <!-- The Team -->
+        <b class="title text-center">The Team</b>
+        <div class="row">
+
+          <!--Abdelrahman col-->
+          <div class="col-md-6 col-sm-6 text-center">
+              <img class="avatar" src="images/avatars/abdlrahman.jpeg" alt="Abdelrahman ...">
+              <h3 class="crimson"><b>Abdelrahman</b></h3>
+              <h3>web developer</h3>
+              <p>Full stack developer with 1 year of experience in Web Industry. front-end skills: HTML, HTML5, CSS, CSS3, jQuery, Bootstrap, photoshop and I'm using php & mysql in the back-end.</p>
+          </div>
+
+          <!--Mahmood col-->
+          <div class="col-md-6 col-sm-6 text-center">
+              <img class="avatar" src="images/avatars/mahmood.png" alt="Mahmood ...">
+              <h3 class="crimson"><b>Mahmood</b></h3>
+              <h3>web developer</h3>
+              <p>I'm building dynamic and responsive websites using HTML, PHP, MySQL, CSS, jQuery, and AJAX. My areas of expertise are in building web apps using PHP, MySQL, Laravel 5, Vue.js and Bootstrap.</p>
+          </div>
+
+        </div>
+        <!-- end The Team -->
+
+    </div><!-- end container-->
+    <!-- ====================== End About Us Content ====================== -->
+
 <?php include('temp/footer.php'); ?>
