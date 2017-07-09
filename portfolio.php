139751fecdf9c67b07180ddd2913e65253f7da43
mahmoodahmad100 <mahmood073@gmail.com>
2017-07-09 22:20:00 +0200

    portfolio page

---
+++ portfolio.php
@@ -0,0 +1,75 @@
+<?php include('temp/header.php'); ?> 
+    <!-- ==================Start Navigate=================== -->
+    <header id="header">
+        <?php include('temp/navigate.php'); ?>
+    </header>
+    <!-- ====================== portfolio Content ====================== -->
+<div class="container gallery-container">
+    <b class="title text-center">latest work</b>
+    <div class="row">
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="images/about.jpg" alt="loading...">
+          <div class="caption">
+            <h3>SpeedySofware</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie laoreet justo, sit amet fringilla sapien accumsan at.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="images/head.jpg" alt="loading...">
+          <div class="caption">
+            <h3>SpeedySofware</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie laoreet justo, sit amet fringilla sapien accumsan at.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="images/contact.jpg" alt="loading...">
+          <div class="caption">
+            <h3>SpeedySofware</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie laoreet justo, sit amet fringilla sapien accumsan at.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="images/about.jpg" alt="loading...">
+          <div class="caption">
+            <h3>SpeedySofware</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie laoreet justo, sit amet fringilla sapien accumsan at.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="images/head.jpg" alt="loading...">
+          <div class="caption">
+            <h3>SpeedySofware</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie laoreet justo, sit amet fringilla sapien accumsan at.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="images/contact.jpg" alt="loading...">
+          <div class="caption">
+            <h3>SpeedySofware</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque molestie laoreet justo, sit amet fringilla sapien accumsan at.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>    
+</div>    
+
+    <!-- ====================== End portfolio Content ====================== -->
+<?php include('temp/footer.php'); ?>
